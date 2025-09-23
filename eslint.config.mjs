d18f13b5878e30f317d7ff7cf1cc1900ec46f8e9
Inam <mfihassan99@gmail.com>
2025-09-23 23:35:09 +0530

    chore: initial setup | remove unnecessary boilerplate code

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,25 +1,33 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import prettier from "eslint-config-prettier";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+  baseDirectory: __dirname
 });
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "react/no-unescaped-entities": "off",
+      "react/prop-types": "off"
+    }
+  },
   {
     ignores: [
       "node_modules/**",
       ".next/**",
       "out/**",
       "build/**",
-      "next-env.d.ts",
-    ],
+      "next-env.d.ts"
+    ]
   },
+  prettier
 ];
 
 export default eslintConfig;
